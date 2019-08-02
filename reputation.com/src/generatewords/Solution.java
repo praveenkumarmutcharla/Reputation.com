1216eb14e5534fff81f2780512f00a91ef44856f
MUTCHARLA PRAVEEN KUMAR <praveenkumarmutcharla94@gmail.com>
2019-08-02 09:46:34 +0530

    Update Solution.java

--- reputation.com/src/generatewords/Solution.java
+++ reputation.com/src/generatewords/Solution.java
@@ -29,9 +29,9 @@ public class Solution {
     result.add("");
     for(char ch:input)
         result = getPermutations(result,map[ch-'2']);
-    return result;
-    
+    return result;    
     }
+    
     private List<String> getPermutations(List<String> templist,char[] maparr)
     {
         List<String> tempresult = new ArrayList<String>();
@@ -40,34 +40,38 @@ public class Solution {
             	tempresult.add(str+ch);
         return tempresult;
     }
-   
-	public List<String> wordsInDictionary( List<String> computedWords) throws IOException
-    {
-    	Iterator<String> iterator = computedWords.iterator();
-    	List<String> meaningFullWords = new ArrayList<String>();
-    	File fileDescriptor;
-    	FileReader fr;
-    	BufferedReader br;
-    	String word=null;
-    	while(iterator.hasNext())
-    	{
-    		String wordToCompare = iterator.next();
-    		fileDescriptor=new File("D:\\STSworkspace\\reputation.com\\google-10000-english.txt");
-        	fr = new FileReader(fileDescriptor);
-    		br = new BufferedReader(fr);   		 
-    		 while((word=br.readLine())!=null)   
-    	      {    	   
-    	         if (word.equals(wordToCompare))
-    	         {
-    	        	 meaningFullWords.add(wordToCompare);
-    	        	 break;
-    	         }
-    	         	
-    	      }
-    	}
+    
+   private List<String> transferfileDataToList() throws IOException
+   {
+   	File fileDescriptor;
+	FileReader fr;
+	BufferedReader br;
+	fileDescriptor=new File("\\STSworkspace\\reputation.com\\google-10000-english.txt");
+	fr = new FileReader(fileDescriptor);
+	br = new BufferedReader(fr);
+	List<String> fileData = new ArrayList<String>();
+	String word=null;
+	 while((word=br.readLine())!=null)   
+	      {    	   
+	         fileData.add(word);	         	
+		  }
+	 br.close();
+	   
+	return fileData;   
+   }
+	public List<String> wordsInDictionary( List<String> fileData,List<String> computedWords) throws IOException
+   	 {
+		Iterator<String> iterator = computedWords.iterator();
+		List<String> meaningFullWords = new ArrayList<String>();
+		while(iterator.hasNext())
+		{
+			String computedWord = iterator.next();
+			if(fileData.contains(computedWord))
+				meaningFullWords.add(computedWord);    					
+		}
     	
 		return meaningFullWords;    	
-    }
+   	 }
     public static void main(String []args) throws IOException
     {
     	Scanner scan = new Scanner(System.in);
@@ -75,16 +79,15 @@ public class Solution {
     	System.out.println("Enter the number where each digit is greater than 1");
     	Number = scan.next();
     	Solution solObj = new Solution();
-    	List<String> computedWords,meaningFullWords;
+    	List<String> computedWords,meaningFullWords,fileData;
     	computedWords = solObj.computeWords(Number);
-    	meaningFullWords = solObj.wordsInDictionary(computedWords);
+    	fileData = solObj.transferfileDataToList();
+    	meaningFullWords = solObj.wordsInDictionary(fileData,computedWords);
     	if(meaningFullWords.size()> 0)
     		System.out.println(meaningFullWords);
     	else
     		System.out.println("No meaning full words present");
-        scan.close();
-    	
-    	
-    }
-        
+        scan.close();	    
+    }       
 }
+
